b03eb60908ed941e2aae47426f3f406dbf3eada2
MuthonduG <muthondugithinji@gmail.com>
2024-08-20 09:30:02 +0300

    update: final commit for servicess && current and final commit for services card

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,8 +1,17 @@
+import Servicescard from "../components/Servicescard"
+import { services } from "../constants"
+
 const Services = () => {
   return (
-    <div>
-      
-    </div>
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {
+        services.map((service) => {
+          return (
+            <Servicescard key={service.label} {...service}/>
+          )
+        })
+      }
+    </section>
   )
 }
 
